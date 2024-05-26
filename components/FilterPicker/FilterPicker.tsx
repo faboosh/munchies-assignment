d4c514b96ea261e98fa05e297c7977eea22a8170
Fabian Johansson <me@faboosh.dev>
2024-05-26 15:49:50 +0200

    Add FilterPicker and WIP RestaurantList components

---
+++ components/FilterPicker/FilterPicker.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useFilterContext } from "@/context/FilterContext";
+import React from "react";
+import styles from "./FilterPicker.module.css";
+import BadgeGroup from "../BadgeGroup";
+import Badge from "../Badge";
+
+export default function FilterPicker() {
+  const {
+    categories,
+    categoryIsSelected,
+    toggleCategory,
+    priceRanges,
+    priceRangeIsSelected,
+    togglePriceRange,
+    deliveryTimes,
+    deliveryTimeIsSelected,
+    toggleDeliveryTime,
+  } = useFilterContext();
+
+  return (
+    <div className={styles.wrapper}>
+      <h2 className="fs-h1">Filter</h2>
+      <BadgeGroup title="FOOD CATEGORY">
+        {categories.map((category) => (
+          <Badge
+            key={category.id}
+            selected={categoryIsSelected(category)}
+            onClick={() => toggleCategory(category)}
+          >
+            {category.name}
+          </Badge>
+        ))}
+      </BadgeGroup>
+      <BadgeGroup title="DELIVERY TIME">
+        {Object.entries(deliveryTimes).map(([_key, deliveryTime]) => (
+          <Badge
+            key={deliveryTime.id}
+            selected={deliveryTimeIsSelected(deliveryTime.id)}
+            onClick={() => toggleDeliveryTime(deliveryTime.id)}
+          >
+            {deliveryTime.text}
+          </Badge>
+        ))}
+      </BadgeGroup>
+      <BadgeGroup title="PRICE RANGE">
+        {priceRanges.map((priceRange) => (
+          <Badge
+            small
+            key={priceRange.id}
+            selected={priceRangeIsSelected(priceRange)}
+            onClick={() => togglePriceRange(priceRange)}
+          >
+            {priceRange.range}
+          </Badge>
+        ))}
+      </BadgeGroup>
+    </div>
+  );
+}
---
+++ components/RestaurantList/RestaurantList.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./RestaurantList.module.css";
+
+export default function RestaurantList() {
+  return <div className={styles.wrapper}></div>;
+}
