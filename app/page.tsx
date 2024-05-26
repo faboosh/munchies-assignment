4c642e8903a83ac26e8077c92de7a94cc8f038aa
Fabian Johansson <me@faboosh.dev>
2024-05-26 21:15:21 +0200

    Convert to grid layout Adjust breakpoints Add sizes

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,28 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import FilterPicker from "@/components/FilterPicker";
 import RestaurantList from "@/components/RestaurantList";
+import CategoryTopBar from "@/components/CategoryTopBar";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.sidebar}>
-        <FilterPicker />
-      </div>
-      <div className={styles.content}>
-        <h2 className="fs-display">Restaurant's</h2>
-        <RestaurantList />
-      </div>
-    </main>
+    <>
+      <main className={styles.main}>
+        <div className={styles.topbar}>
+          <img
+            alt="Munchies logo"
+            src="/logo.svg"
+            className={styles.topbar__image}
+          />
+        </div>
+        <div className={styles.sidebar}>
+          <FilterPicker />
+        </div>
+        <div className={styles.content}>
+          <CategoryTopBar />
+          <h2 className="fs-display">Restaurants</h2>
+          <RestaurantList />
+        </div>
+      </main>
+    </>
   );
 }
